905e19aed652e4a799b619c3a37661e38988c2db
Alexander Mischuk <mischuk.alexander@gmail.com>
2016-12-02 12:21:02 +0200

    Initial project

---
+++ dest/js/app.js
@@ -0,0 +1,43 @@
+$(function() {
+    $('a[href="#"]').click(function(e){ e.preventDefault(); });
+
+    /*! Mask for form's input */
+    function inputMask() {
+      $(".mask-date").mask("99.99.9999",{placeholder:"__.__.____"});
+      $(".mask-year").mask("9999",{placeholder:""});
+      $(".mask-tel").mask("+7 (999) 999-99-99",{placeholder:"X"});
+    };
+    inputMask();
+
+    console.log('Initial header.js');
+
+    
+
+    function popups() {
+
+      $('.call-popup').magnificPopup({
+
+        type: 'inline',
+
+        fixedContentPos: false,
+
+        fixedBgPos: true,
+
+        overflowY: 'auto',
+
+        closeBtnInside: true,
+
+        preloader: false,
+
+        midClick: true,
+
+        removalDelay: 300,
+
+        mainClass: 'my-mfp-zoom-in'
+
+      });
+
+    };
+
+    popups();
+});
---
+++ source/modules/popups/m_popups.js
@@ -0,0 +1,14 @@
+function popups() {
+  $('.call-popup').magnificPopup({
+    type: 'inline',
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+  });
+};
+popups();
