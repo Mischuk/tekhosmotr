607a0845126e0807483f6113a6def91b1a425eaa
Alexander Mischuk <mischuk.alexander@gmail.com>
2016-12-02 23:16:55 +0200

    Finish

--- source/static/js/main.js
+++ source/static/js/main.js
@@ -5,9 +5,71 @@ $(function() {
     function inputMask() {
       $(".mask-date").mask("99.99.9999",{placeholder:"__.__.____"});
       $(".mask-year").mask("9999",{placeholder:""});
-      $(".mask-tel").mask("+7 (999) 999-99-99",{placeholder:"X"});
+      $(".mask-tel").mask("+7 (999) 999 99 99",{placeholder:""});
     };
     inputMask();
+
+    $(document).on('click', '.ripple', function(e) {
+      var $rippleElement = $('<span class="ripple-effect" />'),
+        $buttonElement = $(this),
+        btnOffset = $buttonElement.offset(),
+        xPos = e.pageX - btnOffset.left,
+        yPos = e.pageY - btnOffset.top,
+        size = parseInt(Math.min($buttonElement.height(), $buttonElement.width()) * 0.5),
+        animateSize = parseInt(Math.max($buttonElement.width(), $buttonElement.height()) * Math.PI);
+
+      $rippleElement
+        .css({
+          top: yPos,
+          left: xPos,
+          width: size,
+          height: size,
+
+          backgroundColor: $buttonElement.data("ripple-color")
+        })
+        .appendTo($buttonElement)
+        .animate({
+          width: animateSize,
+          height: animateSize,
+          opacity: 0
+        }, 500, function() {
+          $(this).remove();
+        });
+    });
+
+    function leadCarousel() {
+      $('.carousel-lead').slick({
+        arrows: false,
+        dots: false
+      });
+    }
+    leadCarousel();
+
+    function anchor() {
+      $('.buttons a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top - 50
+            }, 500);
+            return false;
+          }
+        }
+      });
+    };
+    anchor();
+
+    $('body').on('click', '.customWindow .call-popup', function(){
+      $('#tekcl').trigger('click');
+    });
+
+    $(window).load(function(){
+      $('.mfp-hide').css({
+        'display': 'none'
+      });
+    });
 
     //=include modules.js
 });
